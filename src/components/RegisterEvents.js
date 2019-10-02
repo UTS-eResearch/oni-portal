bc90bd32336579cd328602af87ed226256f41ec6
Moises Sacal <moisbo@gmail.com>
2019-10-02 15:59:07 +1000

    removed unused code

--- src/components/RegisterEvents.js
+++ src/components/RegisterEvents.js
@@ -1,50 +1,23 @@
-const Header = require('./views/header');
-const Main = require('./views/main');
-const Footer = require('./views/footer');
-const Search = require('./views/search');
-const ViewDoc = require('./views/ViewDoc');
-const ViewError = require('./views/ViewError');
 const delegate = require('./Delegate');
-const solrService = require('./SolrService');
 
 const RegisterEvents = function (state) {
-  const renderApp = function (data, into) {
-    into.innerHTML = [Header(data), Search(data), Main(data), Footer(data)].join('');
-  };
-
-  const app = document.querySelector('#app');
 
   delegate('#app', 'click', '#search-text', async () => {
     const search = document.querySelector('#text-to-search');
     window.location.hash = `#search/${state.main.start}/${state.main.page}/${search.value}`;
-
-    // const {data, status} = await solrService.search({api: state.config.api}, {start: state.main.start, page: state.main.page, text: search.value});
-    // state.main.docs = data.docs;
-    // state.main.numFound = data.numFound;
-    // renderApp(state, app);
   });
 
   delegate('#app', 'keyup', '#text-to-search', async (event) => {
     if (event.keyCode === 13) {
       event.preventDefault();
-
       const search = document.querySelector('#text-to-search');
       window.location.hash = `#search/${state.main.start}/${state.main.page}/${search.value}`;
-
-      // const {data, status} = await solrService.search({api: state.config.api}, {start: state.main.start, page: state.main.page, text: search.value});
-      // state.main.docs = data.docs;
-      // state.main.numFound = data.numFound;
-      // renderApp(state, app);
     }
   });
 
   delegate('#app', 'click', '#search-text-1', async () => {
     const search = document.querySelector('#text-to-search');
     window.location.hash = `#search/${state.main.start}/${state.main.page}/${search.value}`;
-
-    // const {data, status} = await solrService.search({api: state.config.api}, {start: state.main.start, page: state.main.page, text: search.value});
-    // state.main.docs = data;
-    // renderApp(state, app);
   });
 };
 
