5708d2b95c68d73e1ed384db986d5e8ad6619064
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-06-29 17:17:19 +1000

    Tidied up the view-single-search-result page a bit

--- src/components/views/ViewDoc.js
+++ src/components/views/ViewDoc.js
@@ -12,7 +12,7 @@ const ViewDoc = {
     return `
 
   <div class="col-8">
-    <div class="item-link">${data.main.doc.name}</div>
+    <div class="item-title">${data.main.doc.name}</div>
     ${ViewDoc.link(data, data.main.doc)}
     ${ViewTable(data, data.main.doc, data.results.viewFields).html()}
   </div>
@@ -20,20 +20,23 @@ const ViewDoc = {
   },
 
   summary: function (data) {
-    let html = '<div class="col-3 docSummary">';
+    let html = '<div class="col-3">';
     let doc = data.main.doc;
     let fields = data.results.summaryFields;
     let facetcf = data.facets;
     for( let fieldcf of fields ) {
       const field = fieldcf['field'];
       const values = Array.isArray(doc[field]) ? doc[field]: [ doc[field] ];
+      if( fieldcf['label'] ) {
+        html += `<div class="summaryHeader">${fieldcf['label']}</div>`;
+      }
       if( fieldcf['facet'] ) {
         for( let facetLink of values.map((v) => Facets.link(data, fieldcf['facet'], v)) ) {
           html += `<div class="summaryField">${facetLink}</div>`
         }
       } else {
         for( let v of values ) {
-          html += `<div class="summaryField">${doc[field]}</div>`;
+          html += `<div class="summaryField">${v}</div>`;
         } 
       }
     }
@@ -45,7 +48,7 @@ const ViewDoc = {
   link: function (data, doc) {
 
     if( data.config.results.viewLinks ) {
-      let html = '<div class="col-3 docLink">';
+      let html = '<div class="resultLink">';
       const linkText = data.config.results.linkText || 'Dataset';
       if (isIterable(doc['uri_id'])) {
         for (let uri_id of doc['uri_id']) {
