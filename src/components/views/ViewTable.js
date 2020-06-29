c3e990b445b9bffe0cfc1567ce7fc13ffba856ee
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-06-29 12:05:20 +1000

    Bit of a hack to render JSON authors in the dataset summary pages

--- src/components/views/ViewTable.js
+++ src/components/views/ViewTable.js
@@ -1,4 +1,5 @@
 const $ = require("jquery");
+
 const ViewSubDoc = require('./ViewSubDoc');
 const SubDocHorizontal = require('./SubDocHorizontal');
 const SubDocDate = require('./SubDocDate');
@@ -48,13 +49,13 @@ const ViewTable = function (data, doc, fields) {
       default:
         if(doc[sdcf.field]) {
           const row = $('<div class="row">');
-          console.log(`field ${sdcf.field} = ${JSON.stringify(doc[sdcf.field])}`);
+          const valueHtml = renderValue(data, sdcf, doc);
           if( sdcf.label ) {
             const label = $('<div class="col-sm-2">').html(sdcf.label);
-            const value = $('<div class="col-sm-6">').html(doc[sdcf.field]);
+            const value = $('<div class="col-sm-6">').html(valueHtml);
             row.append(label).append(value);
           } else {
-            const value = $('<div class="col-sm-8">').html(doc[sdcf.field]);
+            const value = $('<div class="col-sm-8">').html(valueHtml);
             row.append(value);
           }
           list.append(row);
@@ -64,5 +65,27 @@ const ViewTable = function (data, doc, fields) {
   }
   return div;
 };
+
+// FIXME - should use same code as Facets
+
+function renderValue(data, sdcf, doc) {
+  if( sdcf.JSON ) {
+      try {
+        const json = JSON.parse('[' + doc[sdcf.field] + ']');
+        if( Array.isArray(json) ) {
+          return json.map((i) => i['display']).join(", ");
+        } else {
+          return json['display']
+        }
+      } catch(e) {
+        console.log(`JSON value parse error on ${sdcf.field} "${doc[sdcf.field]}": ${e}`);
+        return '';
+      }
+  } else {
+    return doc[sdcf.field];
+  }
+}
+
+
 
 module.exports = ViewTable;
