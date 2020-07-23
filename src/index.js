b29387bbf5bbd89a42ef8ea74931502ed6e1a5f5
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-07-23 16:48:35 +1000

    First version which reads the portal config from the express app and so doesn't need rebuilding. Still a bit buggy - facets in pages are broken - but not too bad

--- src/index.js
+++ src/index.js
@@ -2,51 +2,51 @@ import 'bootstrap';
 import './styles/styles.scss';
 import './images/logo.svg';
 
+const axios = require('axios');
 const config = require('../config.json');
 
 //App view components
 const Router = require('./components/Router');
 const RegisterEvents = require('./components/RegisterEvents');
 
-// Default state
-// config is passed through from the oni-express app
 
-// TODO - most of the config in state should be moved out to 
-// config.json
-
-let state = {
-  apis: config.apis,
-  header: config.header,
-  footer: config.footer,
-  pages: config.pages,
-  search: config.search,
-  results: config.results,
-  errors: config.errors,
-  main: {
-    docs: [],
-    doc: {},
-    start: 0,
-    page: 1,
-    numFound: 0,
-    pageSize: 10,
-    searchText: '',
-    currentSearch: {},
-    related: [],
-  },
-  facets: config.facets,
-
-  facetData: [],
-  facetLimit: 5,
-  config: config
-};
-
-RegisterEvents(state);
 
 window.onhashchange = main;
 document.title = config.header.title;
 
 // Main App
 async function main() {
+  const res = await axios.get('http://localhost:8080/portalcf/config.json');
+  const config = res.data;
+
+  const state = {
+    apis: config.apis,
+    header: config.header,
+    footer: config.footer,
+    pages: config.pages,
+    search: config.search,
+    results: config.results,
+    errors: config.errors,
+    main: {
+      docs: [],
+      doc: {},
+      start: 0,
+      page: 1,
+      numFound: 0,
+      pageSize: 10,
+      searchText: '',
+      currentSearch: {},
+      related: [],
+    },
+    facets: config.facets,
+
+    facetData: [],
+    facetLimit: 5,
+    config: config
+  };
+
+  RegisterEvents(state);
+
   await Router(state);
 }
 
