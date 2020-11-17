f56f507e35638e8e7f01c21687de2353e7d05b9b
Mike Lynch <Michael.Lynch@uts.edu.au>
2020-11-17 12:21:05 +1100

    Moved the recursive version of SubDocSubgraph to be the main one

--- src/components/views/SubDocSubgraph.js
+++ src/components/views/SubDocSubgraph.js
@@ -1,26 +1,90 @@
 const $ = require('jquery');
 
-// very basic subgraph rendering
+// first draft of recursive subgraph rendering
 
-const SubDocSubgraph = function (data) {
+const SubDocSubgraphRecursive = function (data) {
   try {
-    const rows = [];
     const subgraph = JSON.parse(data.value);
-    for( let item of subgraph ) {
-      const id = item['@id'];
-      const name = item['name'];
-      const type = Array.isArray(item['@type']) ? item['@type'][0] : item['@type'];
-      const row = $('<div class="row">');
-      row.append($('<div class="col-sm-2">').html(type));
-      row.append($('<div class="col-sm-6">').html(name));
-      rows.push(row);
-      data.element.append(row);
-    }
-    return rows;
+    renderItem({}, data.element, subgraph, subgraph[0], 2);
   } catch (e) {
-    return [ data.element.append('error displaying element' + data.config.element) ];
+    console.log("Error displaying recursive subgraph");
+    console.log(e);
+    data.element.append($('<p>').html('error displaying element'));
+    data.element.append($('<pre>').html(e));
+    return data.element;
   }
 
 };
 
-module.exports = SubDocSubgraph;
+
+function renderItem(rendered, element, subgraph, item, indent) {
+  rendered[item['@id']] = 1;
+  for( let field in item ) {
+    const value = item[field];
+    const refs = isReflist(value);
+    if( refs ) {
+      for( let ref of refs ) {
+        if( !rendered[ref['@id']] ) {
+          console.log(`Following id ${ref['@id']}`);
+          const subItem = findRef(subgraph, ref['@id']);
+          if( subItem ) {
+            element.append(renderItem(rendered, element, subgraph, subItem, indent + 1));
+          } else {
+            console.log("no match");
+          }
+        }
+      }
+    } else {
+      let strvalue = value;
+      if( typeof(value) === 'object' ) {
+        strvalue = value['name'];
+      }
+      const row = $('<div class="row">');
+      row.append($(`<div class="col-sm-${indent}">`).html(field));
+      row.append($(`<div class="col-sm-${8 - indent}">`).html(strvalue));
+      element.append(row);
+    }
+  }
+}
+
+
+function isReflist(value) {
+  console.log(`isReflist ${JSON.stringify(value)}`);
+  if( Array.isArray(value) && value.length > 0 ) {
+    if( isRef(value[0]) ) {
+      console.log('yes');
+      return value;
+    }
+  } else {
+    if( isRef(value) ) {
+      console.log('yes');
+      return [ value ];
+    }
+  }
+  console.log('no');
+  return false;
+}
+
+function isRef(obj) {
+  console.log(`isRef ${JSON.stringify(obj)}`);
+  if( typeof(obj) === 'object') {
+    if( obj['@id'] ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+function findRef(subgraph, id) {
+  const matches = subgraph.filter((i) => i['@id'] === id);
+  if( matches && matches.length > 0 ) {
+    return matches[0];
+  } else {
+    return undefined;
+  }
+}
+
+
+
+module.exports = SubDocSubgraphRecursive;
